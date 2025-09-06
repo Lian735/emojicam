1ca7d34a7ce759e06314d66b42a12fd5d618b5ff
Lian735 <lianschusterr@icloud.com>
2025-09-06 13:28:50 +0200

    Initial Commit

---
+++ EmojiCam/EmojiCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCamApp.swift
+//  EmojiCam
+//
+//  Created by Lian on 06.09.25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiCamTests/EmojiCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCamTests.swift
+//  EmojiCamTests
+//
+//  Created by Lian on 06.09.25.
+//
+
+import Testing
+@testable import EmojiCam
+
+struct EmojiCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
